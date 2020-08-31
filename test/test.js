dd909e3a1af53103bc9b5c055e5c578b0059007a
Tim Hsiung <timothy.hsiung@gmail.com>
2020-08-31 15:28:34 +0800

    Add sample mocha/chai and some sample tests

---
+++ test/test.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+
+describe('test', () => {
+    it('more tests', () => {
+      expect('me').to.equal( 'me');
+    });
+
+    it('more tests for you', () => {
+      expect('you').to.equal( 'you');
+    });
+});
