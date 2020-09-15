b85d093c4a634309dd9e5a8c3505c41d7d438493
Tim Hsiung <timothy.hsiung@gmail.com>
2020-09-15 12:35:00 +0800

    Updated seed to run with async/await

--- src/data/seed.js
+++ src/data/seed.js
@@ -8,53 +8,58 @@ const StationInfo = require('../models/stationInfo');
 let jsonFilePath = path.join(__dirname, 'YouBikeTP.json');
 //jsonFilePath = 'YouBikeTP-min.json';
 
-if (Object.keys(mongoose.connection.collections).includes('stationinfos')) {
-    mongoose.connection.dropCollection('stationinfos');
-    console.log('Deleting stationinfos collection');
-}
-
 const contents = fs.readFileSync(jsonFilePath);
-
 let jsonObject = JSON.parse(contents);
 let stations = Object.values(jsonObject.retVal);
 
-stations.forEach(async (station) => {
-    const stationInfo = new StationInfo({
-        stationId: station.sno,
-        location: {
-            type: 'Point',
-            coordinates: [
-                station.lng,
-                station.lat
-            ]
-        },
+async function deleteStations() {
+    await StationInfo.deleteMany({});    
+}
 
-        stationName: station.sna,
-        stationAddress: station.ar,
-        stationArea: station.sarea,
+async function seedStations() {
+    stations.forEach(async (station) => {
+        const stationInfo = new StationInfo({
+            stationId: station.sno,
+            location: {
+                type: 'Point',
+                coordinates: [
+                    station.lng,
+                    station.lat
+                ]
+            },
 
-        stationName_en: station.snaen,
-        stationAddress_en: station.aren,
-        stationArea_en: station.sareaen,
+            stationName: station.sna,
+            stationAddress: station.ar,
+            stationArea: station.sarea,
 
-        numTotalBikeSpots: station.tot,
-        numEmptyBikeSpots: station.bemp,
-        numOccupiedBikeSpots: station.sbi,
+            stationName_en: station.snaen,
+            stationAddress_en: station.aren,
+            stationArea_en: station.sareaen,
 
-        lastUpdated: station.mday,
-        active: station.act,
+            numTotalBikeSpots: station.tot,
+            numEmptyBikeSpots: station.bemp,
+            numOccupiedBikeSpots: station.sbi,
+
+            lastUpdated: station.mday,
+            active: station.act,
+        });
+
+        // console.log(stationInfo);
+
+        await stationInfo.save((err, stn) => {
+            if (err) {
+                console.log(station)
+            }
+
+            // console.log(stn);
+        });
     });
+}
 
-    // console.log(stationInfo);
-
-    await stationInfo.save((err, stn) => {
-        if (err) {
-            console.log(station)
-        }
-
-        // console.log(stn);
-    });
+deleteStations().then(async () => {
+    console.log('Deleted all stations');
+    await seedStations();
+}).then(() => {
+    console.log(`Added ${stations.length} stations`);
+    process.exit(); 
 });
-
-console.log(`Added ${stations.length} stations`);
-process.exit();
