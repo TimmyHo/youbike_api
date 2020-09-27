785d11a71a8be4004e5fd34ee78696e385e33130
Tim Hsiung <timothy.hsiung@gmail.com>
2020-09-27 22:31:22 +0800

    Added to ensure testing doesn't connect to the local/cloud mongo instance

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
-let mongoDbUrl = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/youbike-db';
+if (process.env.NODE_ENV != 'test') {
+    let mongoDbUrl = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/youbike-db';
 
-mongoose.connect(mongoDbUrl, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-});
+    mongoose.connect(mongoDbUrl, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    });
+}
