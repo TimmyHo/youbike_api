f972387de1a752892ddc3d26abd7b603f519cd13
Tim Hsiung <timothy.hsiung@gmail.com>
2020-10-06 12:23:27 +0800

    Added a search nearby as well as a search by text for youbike

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -1,17 +1,21 @@
+const searchTextInput = document.querySelector('#searchTextInput');
+const searchTextButton = document.querySelector('#searchTextButton');
 
-const searchButton = document.querySelector('#searchButton');
+const searchNearbyButton = document.querySelector('#searchNearbyButton');
 const infoText = document.querySelector('#infoText');
 
 const stationSource = document.getElementById("station-template").innerHTML;
 const stationTemplate = Handlebars.compile(stationSource);
 
-searchButton.addEventListener('click', (e) => {
-    const url = '/api/stations/list?size=3';
+searchTextButton.addEventListener('click', (e) => {
+    searchText = searchTextInput.value
+    const url = `/api/stations/search?q=${searchText}`
 
     fetch(url).then(response => {
         response.json().then((data) => {
+            infoText.innerHTML = ''
             data.stations.forEach(station => {
-                console.log(station)
+                // console.log(station)
                 infoText.insertAdjacentHTML('beforeend', stationTemplate(station));
             });
             
@@ -19,4 +23,25 @@ searchButton.addEventListener('click', (e) => {
             // console.log(data.stations);
         });
     });
+})
+
+searchNearbyButton.addEventListener('click', (e) => {
+    navigator.geolocation.getCurrentPosition((geolocation) => {
+        console.log(geolocation);
+        const coords = geolocation.coords
+        const url = `/api/stations/nearby?loc=${coords.latitude},${coords.longitude}`
+        console.log(url);
+        fetch(url).then(response => {
+            response.json().then((data) => {
+                infoText.innerHTML = ''
+                data.stations.forEach(station => {
+                    // console.log(station)
+                    infoText.insertAdjacentHTML('beforeend', stationTemplate(station))
+                });
+                
+                // infoText.innerHTML = JSON.stringify(data.stations);
+                 console.log(data.stations);
+            });
+        });
+    })
 })
