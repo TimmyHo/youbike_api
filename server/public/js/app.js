e108c1a961f2bb36347f7310a6637c79e5bffcdf
Tim Hsiung <timothy.hsiung@gmail.com>
2020-10-07 14:02:55 +0800

    UI improvements - Redid layout of the page - Changed icons to vary based on station status - Redesigned UI for R side station list to use bootstrap card

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -6,6 +6,8 @@ initMap = () => {
         center: mapCenter,
         zoom: 14,
     });
+
+    refreshMap();
 }
 
 let activeInfoWindow = null;
@@ -13,7 +15,7 @@ let activeInfoWindow = null;
 const searchTextInput = document.querySelector('#searchTextInput');
 const searchTextButton = document.querySelector('#searchTextButton');
 
-const searchNearbyButton = document.querySelector('#searchNearbyButton');
+const refresh = document.querySelector('#refreshButton');
 const infoText = document.querySelector('#infoText');
 
 const stationSource = document.getElementById('station-template').innerHTML;
@@ -22,9 +24,12 @@ const stationTemplate = Handlebars.compile(stationSource);
 const stationInfoWindowSource = document.getElementById('station-info-window-template').innerHTML;
 const stationInfoWindowTemplate = Handlebars.compile(stationInfoWindowSource);
 
-
-Handlebars.registerHelper('timeFromNow', function (date) {
+Handlebars.registerHelper('timeFromNow', (date) => {
     return moment(date).fromNow()
+})
+
+Handlebars.registerHelper('getCardMarkerImage', (isActive, numTotalBikeSpots, numOccupiedBikeSpots, numEmptyBikeSpots) => {
+    return getMarkerImage({isActive, numTotalBikeSpots, numOccupiedBikeSpots, numEmptyBikeSpots})
 })
 
 searchTextButton.addEventListener('click', (e) => {
@@ -48,7 +53,80 @@ searchTextButton.addEventListener('click', (e) => {
 })
 
 
-searchMapButton.addEventListener('click', (e) => {
+refreshButton.addEventListener('click', (e) => {
+    refreshMap();
+})
+
+
+// const searchNearbyButton = document.querySelector('#searchNearbyButton');
+// searchNearbyButton.addEventListener('click', (e) => {
+//     navigator.geolocation.getCurrentPosition((geolocation) => {
+//         console.log(geolocation);
+//         const coords = geolocation.coords
+//         const url = `/api/stations/nearby?loc=${coords.latitude},${coords.longitude}`
+//         console.log(url);
+//         fetch(url).then(response => {
+//             response.json().then((data) => {
+//                 infoText.innerHTML = ''
+//                 data.stations.forEach(station => {
+//                     // console.log(station)
+//                     infoText.insertAdjacentHTML('beforeend', stationTemplate(station))
+//                 });
+                
+//                 // infoText.innerHTML = JSON.stringify(data.stations);
+//                  console.log(data.stations);
+//             });
+//         });
+//     })
+// })
+
+
+getMarkerImage = (station) => {
+    let imageUrl = 'img/icon_nomo.png'
+
+    if (station.active === 0) {
+        imageUrl = 'img/icon_service.png'
+    } else if (station.numEmptyBikeSpots === 0) {
+        imageUrl = 'img/icon_nobike.png'
+    } else if (station.numOccupiedBikeSpots === station.numTotalBikeSpots) {
+        imageUrl = 'img/icon_full.png'
+    }
+
+    return imageUrl
+}
+
+generateMapMarkers = (stations) => {
+    stations.forEach(station => {
+        const coords = {
+            lat: parseFloat(station.location.coordinates[1]), 
+            lng: parseFloat(station.location.coordinates[0]) 
+        }
+        
+        const markerImage = getMarkerImage(station)
+        const marker = new google.maps.Marker({
+            position: coords,
+            title: `#${station.stationId}: ${station.stationName_en}`,
+            icon: markerImage
+        });
+
+        const infoWindow = new google.maps.InfoWindow({
+            content: stationInfoWindowTemplate(station),
+        });
+
+        marker.addListener("click", () => {
+            if (activeInfoWindow !== null) {
+                activeInfoWindow.close();
+            }
+
+            activeInfoWindow = infoWindow;
+            activeInfoWindow.open(googleMap, marker);
+        });
+
+        marker.setMap(googleMap)
+    });
+}
+
+refreshMap = () => {
     const mapCenter = googleMap.getCenter();
 
     const coords = {
@@ -72,59 +150,4 @@ searchMapButton.addEventListener('click', (e) => {
                 console.log(data.stations);
         });
     });
-})
-
-searchNearbyButton.addEventListener('click', (e) => {
-    navigator.geolocation.getCurrentPosition((geolocation) => {
-        console.log(geolocation);
-        const coords = geolocation.coords
-        const url = `/api/stations/nearby?loc=${coords.latitude},${coords.longitude}`
-        console.log(url);
-        fetch(url).then(response => {
-            response.json().then((data) => {
-                infoText.innerHTML = ''
-                data.stations.forEach(station => {
-                    // console.log(station)
-                    infoText.insertAdjacentHTML('beforeend', stationTemplate(station))
-                });
-                
-                // infoText.innerHTML = JSON.stringify(data.stations);
-                 console.log(data.stations);
-            });
-        });
-    })
-})
-
-
-generateMapMarkers = (stations) => {
-    stations.forEach(station => {
-        let coords = {
-            lat: parseFloat(station.location.coordinates[1]), 
-            lng: parseFloat(station.location.coordinates[0]) 
-        }
-        
-        console.log(station);
-        const marker = new google.maps.Marker({
-            position: coords,
-            title: station.stationName_en,
-            icon: "img/youbike_marker.png"
-        });
-
-        const infoWindow = new google.maps.InfoWindow({
-            content: stationInfoWindowTemplate(station),
-        });
-
-        marker.addListener("click", () => {
-            if (activeInfoWindow !== null) {
-                activeInfoWindow.close();
-            }
-
-            activeInfoWindow = infoWindow;
-            activeInfoWindow.open(googleMap, marker);
-        });
-
-        marker.setMap(googleMap)
-    });
-
-    
 }
